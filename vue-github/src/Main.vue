7668d47834e449bbb39cc1343f4484c811d6ff36
takayukioda <takayuki@odashi.me>
2019-07-31 01:25:05 +0900

    wip: start working on sending request

--- vue-github/src/Main.vue
+++ vue-github/src/Main.vue
@@ -10,6 +10,15 @@
 			name="token"
 			v-model="token"
 		>
+		<label for="repository">
+			Repository
+		</label>
+		<input
+			type="text"
+			name="repository"
+			placeholder=":owner/:repo"
+			v-model="repository"
+		>
 		<label for="title">
 			Issue title
 		</label>
@@ -35,15 +44,28 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-	data () {
+	data() {
 		return {
+			loading: false,
 			token: '',
 			title: '',
 			body: '',
+			repository: '',
 		}
 	},
-	async asyncData() {
+	methods: {
+		async createIssue() {
+			if (!this.token || !this.repository) {
+				// eslint-disable-next-line
+				console.error("No token or repository has been set")
+				return
+			}
+			this.loading = true
+			axios.post(`https://api.github.com/repos/${this.repository}/issues`)
+		}
 	},
 }
 </script>
