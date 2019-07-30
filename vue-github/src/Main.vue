e97c7e0cbc12d5440be33217d0529ee404b3c76c
takayukioda <takayuki@odashi.me>
2019-07-31 01:41:46 +0900

    fix: remove form to avoid sending form request

--- vue-github/src/Main.vue
+++ vue-github/src/Main.vue
@@ -1,46 +1,44 @@
 <template>
 <main id="#main">
 	<h1>GitHub Issue Maker</h1>
-	<form>
-		<label for="token">
-			GitHub TOKEN
-		</label>
-		<input
-			type="text"
-			name="token"
-			v-model="token"
-		>
-		<label for="repository">
-			Repository
-		</label>
-		<input
-			type="text"
-			name="repository"
-			placeholder=":owner/:repo"
-			v-model="repository"
-		>
-		<label for="title">
-			Issue title
-		</label>
-		<input
-			type="text"
-			name="title"
-			v-model="title"
-		>
-		<label for="body">
-			Issue body
-		</label>
-		<textarea
-			name="body"
-			v-model="body"
-		>
-		</textarea>
-		<input
-			type="submit"
-			value="Make an issue"
-			v-on:click="createIssue"
-		>
-	</form>
+	<label for="token">
+		GitHub TOKEN
+	</label>
+	<input
+		type="text"
+		name="token"
+		v-model="token"
+	>
+	<label for="repository">
+		Repository
+	</label>
+	<input
+		type="text"
+		name="repository"
+		placeholder=":owner/:repo"
+		v-model="repository"
+	>
+	<label for="title">
+		Issue title
+	</label>
+	<input
+		type="text"
+		name="title"
+		v-model="title"
+	>
+	<label for="body">
+		Issue body
+	</label>
+	<textarea
+		name="body"
+		v-model="body"
+	>
+	</textarea>
+	<button
+		v-on:click="createIssue"
+	>
+		Make an issue
+	</button>
 </main>
 </template>
 
@@ -58,21 +56,21 @@ export default {
 		}
 	},
 	methods: {
-		async createIssue() {
+		createIssue() {
 			if (!this.token || !this.repository) {
 				// eslint-disable-next-line
 				console.error("No token or repository has been set")
 				return
 			}
 			this.loading = true
-			const resp = await axios.post(`https://api.github.com/repos/${this.repository}/issues`, {
+			axios.post(`https://api.github.com/repos/${this.repository}/issues`, {
 				headers: {
 					Authorization: this.token,
 				},
+			}).then((resp) => {
+				// eslint-disable-next-line
+				console.log(resp.data)
 			})
-
-			// eslint-disable-next-line
-			console.log(resp.data)
 		}
 	},
 }
