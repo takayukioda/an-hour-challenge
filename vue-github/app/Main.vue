f962381725f2b86d0566d75c35983a9ec902661b
takayukioda <takayuki@odashi.me>
2019-07-31 00:42:32 +0900

    wip: try make vue file

---
+++ vue-github/app/Main.vue
@@ -0,0 +1,39 @@
+<template>
+<main>
+	<h1>GitHub Issue Maker</h1>
+	<form>
+		<label for="token">
+			GitHub TOKEN
+		</label>
+		<input
+			type="text"
+			name="token"
+			v-model="token"
+		>
+		<label for="title">
+			Issue title
+		</label>
+		<input
+			type="text"
+			name="title"
+			v-model="title"
+		>
+		<label for="body">
+			Issue body
+		</label>
+		<textarea
+			name="body"
+			v-model="body"
+		>
+		</textarea>
+		<input
+			type="submit"
+			value="Make an issue"
+		>
+	</form>
+</main>
+</template>
+
+<script>
+
+</script>
